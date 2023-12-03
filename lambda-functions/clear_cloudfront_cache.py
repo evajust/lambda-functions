73c7f65cc868d2460232125b4d9cbb3115965e29
EC2 Default User <ec2-user@ip-10-0-0-58.ec2.internal>
2023-12-03 04:03:14 +0000

    Starting to get this ready as a lambda function

--- lambda-functions/clear_cloudfront_cache.py
+++ lambda-functions/clear_cloudfront_cache.py
@@ -3,21 +3,35 @@
 import boto3
 import sys
 from datetime import datetime
+import json
+import logging
 
-client = boto3.client('cloudfront')
 
-caller_reference = datetime.now().strftime('%Y%m%d%H%M%S%f')
-url = sys.argv[1]
-invalidation_path = {'Paths': {
-                         'Quantity': 1,
-                         'Items': ['/*']
-                         },
-                     'CallerReference': caller_reference
-                     }
+logger = logging.getLogger(__name__)
 
-distribution_list = client.list_distributions()['DistributionList']['Items']
-for distribution in distribution_list:
-    if url in distribution['Aliases']['Items']:
-        response = client.create_invalidation(
-                DistributionId=distribution['Id'],
-                InvalidationBatch=invalidation_path)
+
+def lambda_handler(event, context):
+    client = boto3.client('cloudfront')
+
+    print("event: " + str(event))
+    print("context: " + str(context))
+    caller_reference = datetime.now().strftime('%Y%m%d%H%M%S%f')
+    url = sys.argv[1]
+    invalidation_path = {'Paths': {
+                             'Quantity': 1,
+                             'Items': ['/*']
+                             },
+                         'CallerReference': caller_reference
+                         }
+
+    response = client.list_distributions()
+    distribution_list = response['DistributionList']['Items']
+    for distribution in distribution_list:
+        if url in distribution['Aliases']['Items']:
+            response = client.create_invalidation(
+                    DistributionId=distribution['Id'],
+                    InvalidationBatch=invalidation_path)
+            return {
+                'statusCode': 200,
+                'body': json.dumps(response)
+            }
